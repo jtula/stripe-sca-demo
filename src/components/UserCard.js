2113b4e81ba02caec7dbb6dc25e85da323727fab
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-01-15 10:41:45 -0500

    fix: update user card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import useUser from "src/hooks/useUser";
+import { Elements } from "@stripe/react-stripe-js";
+import Checkout from "src/components/Checkout";
+import getStripe from "src/utils/get-stripe";
 
 const UserCard = () => {
-  const { user, logout } = useUser();
+  const { token, userLogged, user, logout } = useUser();
   const history = useHistory();
+  const [stripePromise, setStripePromise] = useState(null);
+
+  useEffect(() => {
+    setStripePromise(getStripe({ token }));
+  }, [token]);
 
   const handleLogout = (event) => {
     event.preventDefault();
@@ -19,10 +28,19 @@ const UserCard = () => {
           style={{ maxWidth: "500px", width: "500px" }}
         >
           <div className="card-body">
-            {user && <h3 className="text-center mb-3">{user.fullName}</h3>}
-            <Link to="#" onClick={handleLogout}>
-              Logout
-            </Link>
+            {userLogged && (
+              <div className="d-flex justify-content-center">
+                <h3 className="text-center mb-3">{`${user.fullName}  `}</h3>
+                <Link to="#" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </div>
+            )}
+            <div className="mt-3">
+              <Elements stripe={stripePromise}>
+                <Checkout />
+              </Elements>
+            </div>
           </div>
         </div>
       </div>
