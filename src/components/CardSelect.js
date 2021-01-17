c76547c516bbe00fb08db8002310e47aa6fa5ea6
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-01-16 23:50:50 -0500

    fix: add select style

--- src/components/CardSelect.js
+++ src/components/CardSelect.js
@@ -3,10 +3,15 @@ import React from "react";
 const CardSelect = ({ cards, cardSelected, handleCards }) => {
   return (
     <>
-      <select name="cards-select" value={cardSelected} onChange={handleCards}>
+      <select
+        name="cards-select"
+        className="form-control"
+        value={cardSelected}
+        onChange={handleCards}
+      >
         {cards.map((card) => (
           <option key={card.id} value={card.id}>
-            {card.name}
+            {`xxxx-xxxx-xxxx-${card.card.last4}`}
           </option>
         ))}
       </select>
