9d59df6b53eb8cb1cba37f1258e8b8b5fc7c66c4
Jose A Tula Leyva <joseph.tula@gmail.com>
2021-01-17 14:25:47 -0500

    fix: add select option

--- src/components/CardSelect.js
+++ src/components/CardSelect.js
@@ -9,6 +9,7 @@ const CardSelect = ({ cards, cardSelected, handleCards }) => {
         value={cardSelected}
         onChange={handleCards}
       >
+        <option value="empty">Pay with saved card</option>
         {cards.map((card) => (
           <option key={card.id} value={card.id}>
             {`xxxx-xxxx-xxxx-${card.card.last4}`}
